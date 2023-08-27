9450445e3ff76f0833465210d1cc6f763867500a
JuanZu77 <juanvzubiri77@gmail.com>
2023-08-27 19:55:59 -0300

    update cliente
    Individual improvements. Update pagination styles

--- clientToolVerse-dev/src/components/Pagination/Pagination.jsx
+++ clientToolVerse-dev/src/components/Pagination/Pagination.jsx
@@ -14,34 +14,43 @@ export default function Pagination() {
     dispatch(setCurrentPage(page));
   };
 
-  const renderPageNumbers = [];
-  for (let i = 1; i <= pageNumbers; i++) {
-    renderPageNumbers.push(i);
-  }
+// Calcula el rango de páginas a mostrar
+const visiblePageRange = 5;
+let startPage = Math.max(1, currentPage - Math.floor(visiblePageRange / 2));
+let endPage = Math.min(pageNumbers, startPage + visiblePageRange - 1);
 
-  return (
-    <div className={style.divButtons}>
+if (endPage - startPage + 1 < visiblePageRange) {
+  startPage = Math.max(1, endPage - visiblePageRange + 1);
+}
+
+const renderPageNumbers = Array.from({ length: endPage - startPage + 1 }, (_, index) => startPage + index);
+  
+
+return (
+  <div className={style.divButtons}>
+    <button
+      className={`${style.AnteriorSiguiente} ${currentPage === 1 ? style.buttonDisabled : ""}`}
+      disabled={currentPage === 1}
+      onClick={() => handleClick(currentPage - 1)}
+    >
+      &lt; Anterior
+    </button>
+    {Array.from({ length: endPage - startPage + 1 }, (_, index) => startPage + index).map((number) => (
       <button
-        disabled={currentPage === 1}
-        onClick={() => handleClick(currentPage - 1)}
+        key={number}
+        className={`${style.button} ${currentPage === number ? style.buttonActual : ""}`}
+        onClick={() => handleClick(number)}
       >
-        Anterior
+        {number}
       </button>
-      {renderPageNumbers.map((number) => (
-        <button
-          key={number}
-          className={currentPage === number ? style.button : ""}
-          onClick={() => handleClick(number)}
-        >
-          {number}
-        </button>
-      ))}
-      <button
-        disabled={currentPage === pageNumbers}
-        onClick={() => handleClick(currentPage + 1)}
-      >
-        Siguiente
-      </button>
-    </div>
-  );
+    ))}
+    <button
+      className={`${style.AnteriorSiguiente} ${currentPage === pageNumbers ? style.buttonDisabled : ""}`}
+      disabled={currentPage === pageNumbers}
+      onClick={() => handleClick(currentPage + 1)}
+    >
+      Siguiente &gt;
+    </button>
+  </div>
+);
 }
